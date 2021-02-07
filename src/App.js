bcf276101125a4566d55aa7f7f820d50395f8727
Clément François <clement.francois.developpeur@outlook.com>
2021-02-07 15:40:05 +0100

    Do App.js
    Do the App.js and App.css in order to change the style of the application and create an array containing all the todo stored inside the todo store.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import './App.css';
 import Input from './components/Input/Input'
+import TodoItem from './components/TodoItem/TodoItem';
+
+import { useSelector } from 'react-redux'
+import { selectTodoList } from './features/todoSlice'
 
 function App() {
+  const todoList = useSelector(selectTodoList);
+
   return (
     <div className="app">
-      {/* todo list */}
-        {/* Done */}
-      <Input />
+      <div className="app__container">
+        <div className="app__todoContainer">
+           {
+             todoList.map(value => (
+               <TodoItem 
+                item={value.item} 
+                done={value.done} 
+                id={value.id}
+                key={value.id}
+              />
+             ))
+           }
+        </div>
+
+        <Input />
+      </div>
     </div>
   );
 }
