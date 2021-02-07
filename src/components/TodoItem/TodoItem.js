0aeac914c18146eb3082ea27022cca27bd6c12ea
Clément François <clement.francois.developpeur@outlook.com>
2021-02-07 15:35:24 +0100

    Add TodoItem
    Add the todo item which is a function that handle the state of the todo on the screen

---
+++ src/components/TodoItem/TodoItem.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './TodoItem.css'
+import { Checkbox } from '@material-ui/core'
+
+import { useDispatch } from 'react-redux'
+import { setCheck } from '../../features/todoSlice'
+
+const TodoItem = ({ item, done, id }) => {
+    const dispatch = useDispatch();
+
+    const handleCheck = () => {
+        dispatch(setCheck(id))
+    }
+
+    return (
+        <div className='todoItem'>
+            <Checkbox 
+                checked={done}
+                color='primary'
+                onChange={handleCheck}
+                inputProps={{'aria-label': 'secondary checkbox'}}
+            />
+            <p className={ done && 'todoItem--done' }>{item}</p>
+        </div>
+    )
+}
+
+export default TodoItem
